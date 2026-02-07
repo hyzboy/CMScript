78d41c29de64e72d2d44686f58642a2ec0b038ef
HuYingzhuo(hugo) <hyzboy@gmail.com>
2026-02-07 23:48:10 +0800

    增加字节码测试用例

---
+++ examples/DevilBytecodeTests.cpp
@@ -0,0 +1,63 @@
+#include <hgl/devil/DevilModule.h>
+#include <hgl/devil/DevilBytecode.h>
+#include "DevilBytecodeBuilder.h"
+#include "DevilFunc.h"
+#include <iostream>
+
+using namespace hgl::devil;
+
+int main(int,char **)
+{
+    Module module;
+    BytecodeModule bc_module;
+    BytecodeBuilder builder(&module,&bc_module);
+
+    const char *script=
+        "func main() { "
+        "int a=1; int b=2; "
+        "if(a<b){ b=b+1; } else { b=b-1; } "
+        "return b; "
+        "} "
+        "func looptest() { "
+        "int x=0; "
+        "do { x=x+1; } while(x<3); "
+        "switch(x){ case 3: x=7; break; default: x=9; } "
+        "return x; "
+        "}";
+
+    if(!module.AddScript(script,-1))
+    {
+        std::cout<<"bytecode test: AddScript failed"<<std::endl;
+        return -1;
+    }
+
+    bc_module.SetHostModule(&module);
+
+    const char *funcs[]={"main","looptest"};
+    for(const char *name:funcs)
+    {
+        Func *func=module.GetScriptFunc(name);
+        if(!func)
+        {
+            std::cout<<"bytecode test: missing func "<<name<<std::endl;
+            return -1;
+        }
+
+        if(!builder.BuildAndAdd(func))
+        {
+            std::cout<<"bytecode test: build failed "<<builder.GetError()<<std::endl;
+            return -1;
+        }
+    }
+
+    const std::string disasm=DisassembleModule(bc_module);
+    if(disasm.empty())
+    {
+        std::cout<<"bytecode test: empty disassembly"<<std::endl;
+        return -1;
+    }
+
+    std::cout<<disasm;
+
+    return 0;
+}
--- path_config.cmake
+++ path_config.cmake
@@ -5,4 +5,6 @@ macro(CMScriptSetup CMSCRIPT_ROOT_PATH)
     set(CMSCRIPT_ROOT_SOURCE_PATH ${CMSCRIPT_ROOT_PATH}/src)
 
     include_directories(${CMSCRIPT_ROOT_INCLUDE_PATH})
+
+    include_directories(${CMSCRIPT_ROOT_SOURCE_PATH}/DevilVM)
 endmacro()
